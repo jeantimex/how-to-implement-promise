77e2f3dabd167a37278770ba0a3ed19073dbdcb8
jeantimex <jean.timex@gmail.com>
2018-08-25 16:00:07 -0700

    Code refactor

--- promise.js
+++ promise.js
@@ -1,3 +1,7 @@
+var PENDING = 0;
+var RESOLVED = 1;
+var REJECTED = 2;
+
 var asyncFn = (function() {
   if (
     typeof process === "object" &&
@@ -28,7 +32,7 @@ function Handler(onResolved, onRejected, promise) {
 
 function resolve(promise, value) {
   // 非 pending 状态不可变
-  if (promise._state !== 0) return;
+  if (promise.state !== PENDING) return;
 
   // promise 和 value 指向同一对象
   // 对应 Promise A+ 规范 2.3.1
@@ -42,23 +46,23 @@ function resolve(promise, value) {
   // 如果 value 为 Promise，则使 promise 接受 value 的状态
   // 对应 Promise A+ 规范 2.3.2
   if (value && value instanceof Promise && value.then === promise.then) {
-    var deferreds = promise._deferreds;
+    var callbacks = promise.callbacks;
 
-    if (value._state === 0) {
+    if (value.state === PENDING) {
       // value 为 pending 状态
-      // 将 promise._deferreds 传递 value._deferreds
+      // 将 promise.callbacks 传递 value.callbacks
       // 偷个懒，使用 ES6 展开运算符
       // 对应 Promise A+ 规范 2.3.2.1
-      value._deferreds.push(...deferreds);
-    } else if (deferreds.length !== 0) {
+      value.callbacks.push(...callbacks);
+    } else if (callbacks.length !== 0) {
       // value 为 非pending 状态
       // 使用 value 作为当前 promise，执行 then 注册回调处理
       // 对应 Promise A+ 规范 2.3.2.2、2.3.2.3
-      for (var i = 0; i < deferreds.length; i++) {
-        handleResolved(value, deferreds[i]);
-      }
+      callbacks.forEach(function(callback) {
+        handleResolved(value, callback);
+      });
       // 清空 then 注册回调处理数组
-      value._deferreds = [];
+      value.callbacks = [];
     }
     return;
   }
@@ -69,9 +73,9 @@ function resolve(promise, value) {
     try {
       // 对应 Promise A+ 规范 2.3.3.1
       var then = getThen(value);
-    } catch (err) {
+    } catch (error) {
       // 对应 Promise A+ 规范 2.3.3.2
-      return reject(promise, err);
+      return reject(promise, error);
     }
 
     // 如果 then 是函数，将 value 作为函数的作用域 this 调用之
@@ -88,8 +92,8 @@ function resolve(promise, value) {
             reject(promise, reason);
           }
         );
-      } catch (err) {
-        reject(promise, err);
+      } catch (error) {
+        reject(promise, error);
       }
       return;
     }
@@ -97,61 +101,62 @@ function resolve(promise, value) {
 
   // 改变 promise 内部状态为 `resolved`
   // 对应 Promise A+ 规范 2.3.3.4、2.3.4
-  promise._state = 1;
-  promise._value = value;
+  promise.state = 1;
+  promise.value = value;
 
   // promise 存在 then 注册回调函数
-  if (promise._deferreds.length !== 0) {
-    for (var i = 0; i < promise._deferreds.length; i++) {
-      handleResolved(promise, promise._deferreds[i]);
-    }
+  if (promise.callbacks.length !== 0) {
+    promise.callbacks.forEach(function(callback) {
+      handleResolved(promise, callback);
+    });
     // 清空 then 注册回调处理数组
-    promise._deferreds = [];
+    promise.callbacks = [];
   }
 }
 
 function reject(promise, reason) {
   // 非 pending 状态不可变
-  if (promise._state !== 0) return;
+  if (promise.state !== PENDING) return;
 
   // 改变 promise 内部状态为 `rejected`
-  promise._state = 2;
-  promise._value = reason;
+  promise.state = REJECTED;
+  promise.value = reason;
 
   // 判断是否存在 then(..) 注册回调处理
-  if (promise._deferreds.length !== 0) {
+  if (promise.callbacks.length !== 0) {
     // 异步执行回调函数
-    for (var i = 0; i < promise._deferreds.length; i++) {
-      handleResolved(promise, promise._deferreds[i]);
-    }
-    promise._deferreds = [];
+    promise.callbacks.forEach(function(callback) {
+      handleResolved(promise, callback);
+    });
+    promise.callbacks = [];
   }
 }
 
-function handleResolved(promise, deferred) {
+function handleResolved(promise, callback) {
   // 异步执行注册回调
   asyncFn(function() {
-    var cb = promise._state === 1 ? deferred.onResolved : deferred.onRejected;
+    var cb =
+      promise.state === RESOLVED ? callback.onResolved : callback.onRejected;
 
     // 传递注册回调函数为空情况
     if (cb === null) {
-      if (promise._state === 1) {
-        resolve(deferred.promise, promise._value);
+      if (promise.state === RESOLVED) {
+        resolve(callback.promise, promise.value);
       } else {
-        reject(deferred.promise, promise._value);
+        reject(callback.promise, promise.value);
       }
       return;
     }
 
     // 执行注册回调操作
     try {
-      var res = cb(promise._value);
-    } catch (err) {
-      reject(deferred.promise, err);
+      var res = cb(promise.value);
+    } catch (error) {
+      reject(callback.promise, error);
     }
 
     // 处理链式 then(..) 注册处理函数调用
-    resolve(deferred.promise, res);
+    resolve(callback.promise, res);
   });
 }
 
@@ -161,15 +166,11 @@ function Promise(resolver) {
   }
 
   // promise 状态变量
-  // 0 - pending
-  // 1 - resolved
-  // 2 - rejected
-  this._state = 0;
+  this.state = PENDING;
   // promise 执行结果
-  this._value = null;
-
+  this.value = null;
   // then(..) 注册回调处理数组
-  this._deferreds = [];
+  this.callbacks = [];
 
   // 立即执行 fn 函数
   try {
@@ -188,19 +189,26 @@ function Promise(resolver) {
 }
 
 Promise.prototype.then = function(onResolved, onRejected) {
+  if (
+    (typeof onResolved !== "function" && this.state === RESOLVED) ||
+    (typeof onRejected !== "function" && this.state === REJECTED)
+  ) {
+    return this;
+  }
+
   var res = new Promise(function() {});
   // 使用 onResolved，onRejected 实例化处理对象 Handler
-  var deferred = new Handler(onResolved, onRejected, res);
+  var callback = new Handler(onResolved, onRejected, res);
 
   // 当前状态为 pendding，存储延迟处理对象
-  if (this._state === 0) {
-    this._deferreds.push(deferred);
+  if (this.state === PENDING) {
+    this.callbacks.push(callback);
     return res;
   }
 
   // 当前 promise 状态不为 pending
   // 调用 handleResolved 执行onResolved或onRejected回调
-  handleResolved(this, deferred);
+  handleResolved(this, callback);
 
   // 返回新 promise 对象，维持链式调用
   return res;
